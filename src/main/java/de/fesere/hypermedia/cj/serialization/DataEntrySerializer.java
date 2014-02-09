568119727a5963a4952309f87f29312c31f3fa78
Felipe Sere <felipesere@gmail.com>
2014-02-09 14:44:40 +0100

    Clean up the parameters and method names

--- src/main/java/de/fesere/hypermedia/cj/serialization/DataEntrySerializer.java
+++ src/main/java/de/fesere/hypermedia/cj/serialization/DataEntrySerializer.java
@@ -4,67 +4,74 @@ import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import de.fesere.hypermedia.cj.exceptions.SerializationException;
 import de.fesere.hypermedia.cj.model.data.*;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 
 public class DataEntrySerializer extends StdSerializer<DataEntry> {
+
+    private JsonGenerator jgen;
+
     protected DataEntrySerializer() {
         super(DataEntry.class);
     }
 
     @Override
-    public void serialize(DataEntry value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonGenerationException {
-        if (value instanceof EmptyDataEntry) {
+    public void serialize(DataEntry entry, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonGenerationException {
+        if (entry instanceof EmptyDataEntry) {
             return;
         }
+        this.jgen = jgen;
+
         jgen.writeStartObject();
 
-        writeName(value, jgen);
-        writeValue(value, jgen);
-        writePrompt(value, jgen);
+        writeName(entry.getName());
+        writeValue(entry.getValue());
+        writePrompt(entry.getPrompt());
 
         jgen.writeEndObject();
 
     }
 
-    private void writePrompt(DataEntry value, JsonGenerator jgen) throws IOException {
-        String prompt = value.getPrompt();
-        if (StringUtils.isNotBlank(prompt)) {
-            jgen.writeStringField("prompt", prompt);
-        }
+    private void writeName(String name) throws IOException {
+        jgen.writeStringField("name", name);
     }
 
-    private void writeName(DataEntry value, JsonGenerator jgen) throws IOException {
-        jgen.writeStringField("name", value.getName());
-    }
-
-    private void writeValue(DataEntry value, JsonGenerator jgen) throws IOException {
+    private void writeValue(Object value) throws IOException {
         jgen.writeFieldName("value");
-        writeContent(value, jgen);
+        writeContent(value);
     }
 
 
-    private void writeContent(DataEntry value, JsonGenerator jgen) throws IOException {
-        if (value instanceof StringDataEntry) {
-            StringDataEntry stringEntry = (StringDataEntry) value;
-            jgen.writeString(stringEntry.getValue());
-        } else if (value instanceof NumberDataEntry) {
-            writeNumber((NumberDataEntry) value, jgen);
-        } else if (value instanceof BooleanDataEntry) {
-            jgen.writeBoolean((boolean) value.getValue());
-        } else {
-            jgen.writeString((String) value.getValue());
+    private void writeContent(Object value) throws IOException {
+        if (value instanceof String) {
+            jgen.writeString((String) value);
+        } else if (value instanceof Number) {
+            Number number = (Number) value;
+            writeNumber(number);
+        } else if (value instanceof Boolean) {
+            jgen.writeBoolean((boolean) value);
+        } else if (value == null) {
+            jgen.writeNull();
+        }
+        else {
+            throw new SerializationException("Unspoorted value of type " + value.getClass().getCanonicalName() );
         }
     }
 
-    private void writeNumber(NumberDataEntry value, JsonGenerator jgen) throws IOException {
-        Number number = value.getValue();
+    private void writeNumber(Number number) throws IOException {
         if (number instanceof Double) {
             jgen.writeNumber(number.doubleValue());
         } else {
             jgen.writeNumber(number.intValue());
+        }
+    }
+
+    private void writePrompt(String prompt) throws IOException {
+        if (StringUtils.isNotBlank(prompt)) {
+            jgen.writeStringField("prompt", prompt);
         }
     }
 }
