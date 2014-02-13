4fc01752629dd754b4b4981bacebe111e3c40b0c
Felipe Sere <felipesere@gmail.com>
2014-02-13 16:43:48 +0100

    Split out reflection based code into separate lib

--- src/test/java/de/fesere/hypermedia/cj/annotation/DefaultPerson.java
+++
@@ -1,7 +0,0 @@
-package de.fesere.hypermedia.cj.annotation;
-
-public class DefaultPerson extends BasePerson {
-    public DefaultPerson(String name, int age, int id, boolean isAdmin, double someValue) {
-        super(name, age, id, isAdmin, someValue);
-    }
-}
